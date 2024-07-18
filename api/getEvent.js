366c90e99d8422d6448d469768e52328ae99b68e
MaxenceR26 <maxencedu62_@outlook.fr>
2024-07-18 14:41:13 +0200

    feat: Adding get by ID sys

--- api/getEvent.js
+++ api/getEvent.js
@@ -18,6 +18,22 @@ router.get('/', (req, res) => {
     });
 });
 
+// Route GET pour obtenir un évènement avec un ID en particulier
+router.get('/:id', (req, res) => {
+    const id = req.params.id;;
+
+    const query = 'SELECT `event_title` FROM `events` WHERE `id` = ?';
+
+    connection.query(query, [id], function(err, results) {
+        if (err) {
+            console.log('erreur +> ', err)
+        }
+        console.log("Requête exécutée avec succès:", results);
+        res.status(200).send(results);
+    })
+    console.log("req.params.id:", req.params.id);
+});
+
 // Route POST pour créer un événement
 router.post('/createEvent', (req, res) => {
     const { event_date, event_title } = req.body;
@@ -29,7 +45,7 @@ router.post('/createEvent', (req, res) => {
 
     const query = 'INSERT INTO events (event_date, event_title) VALUES (?, ?)';
 
-    connection.query(query, [event_date, event_title], function(err, results) {
+    connection.query(query, [event_date, event_title], function(err, results, fields) {
         if (err) {
             console.error("Une erreur est survenue:", err);
             res.status(500).send("Une erreur s’est produite lors de l’exécution de la requête.");
